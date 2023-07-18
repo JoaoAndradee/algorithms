03050aad53a88adc824e2582ba5ff2bc3b3d58e1
joao <joao.dev94@gmail.com>
2023-07-18 16:04:46 -0300

    Requisito 01 - Implementação da função que calcula o número de estudantes em determinado horario

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    counter = 0
+    for period in permanence_period:
+        start, end = period
+        if not (isinstance(start, int) and isinstance(end, int)):
+            return None
+        if start <= target_time <= end:
+            counter += 1
+    return counter
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
