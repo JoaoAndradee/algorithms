833ee559bc751c869fd95808800223aec80e06e7
joao <joao.dev94@gmail.com>
2023-07-18 17:54:19 -0300

    Requisito 02 - Adição de testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(Exception):
+        encrypt_message("Message", "Invalid_type")
+
+    with pytest.raises(Exception):
+        encrypt_message(12, 12)
+
+    assert encrypt_message("Mensagem", 0) == "megasneM"
+
+    assert encrypt_message("Mensagem", 20) == "megasneM"
+
+    assert encrypt_message("Mensagem", 1) == "M_megasne"
+
+    assert encrypt_message("Mensagem", 2) == "megasn_eM"
