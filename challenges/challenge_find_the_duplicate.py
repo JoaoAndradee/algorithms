34edd35f37d9fb9102be823254fc0af2aeedf807
joao <joao.dev94@gmail.com>
2023-07-19 02:24:00 -0300

    Requisito 05 - Criação de função para achar números repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (not nums or
+            not any(isinstance(num, int) for num in nums) or
+            len(nums) < 2 or
+            min(nums) < 1):
+        return False
+
+    nums.sort()
+
+    for index in range(1, len(nums)):
+        if nums[index] == nums[index - 1]:
+            return nums[index]
+
+    return False
